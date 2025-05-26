31ec0cd98daf83cf61490a1fc48311f5004d3437
Matt Tolhurst <matt@gffo.xyz>
2025-05-26 20:18:09 +1000

    typo

--- bootstrap/deploy.sh
+++ bootstrap/deploy.sh
@@ -6,7 +6,7 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl apply -n argocd -f ./argocd-repositories.yml
-kubectl apply -n argocd -f ./argocd-repo.creds.yml
+kubectl apply -n argocd -f ./argocd-repo-creds.yml
 
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
