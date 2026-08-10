0855c6ec69396a22eb505012a48f9f84965e1c0c
Matt Tolhurst <matt@gffo.xyz>
2026-08-10 23:46:28 +1000

    fix: helm syntax

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -2,25 +2,6 @@
 
 set -x
 
-helm repo add cilium https://helm.cilium.io/
-helm repo update
-
-helm template \
-    cilium \
-    cilium/cilium \
-    --version 1.20.0 \
-    --namespace kube-system \
-    --set ipam.mode=kubernetes \
-    --set kubeProxyReplacement=true \
-    --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
-    --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
-    --set cgroup.autoMount.enabled=false \
-    --set cgroup.hostRoot=/sys/fs/cgroup \
-    --set k8sServiceHost=localhost \
-    --set k8sServicePort=7445 > cilium.yaml
-
-kubectl apply -f cilium.yaml
-
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
