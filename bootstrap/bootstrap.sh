b16e3b7c599ce7c041c33995c423698dad9b0733
Matt Tolhurst <matt@gffo.xyz>
2026-08-11 11:24:58 +1000

    fix: cilium

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -5,6 +5,14 @@ set -x
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+kubectl patch configmap argocd-cm -n argocd --type merge -p '
+{
+  "data": {
+    "resource.exclusions": "- apiGroups:\n    - cilium.io\n  kinds:\n    - CiliumIdentity\n    - CiliumEndpoint\n    - CiliumEndpointSlice\n  clusters:\n    - \"*\"\n"
+  }
+}'
+
+
 kubectl apply -n argocd -f ./argocd-repositories.yml
 kubectl apply -n argocd -f ./infra.yml
 
